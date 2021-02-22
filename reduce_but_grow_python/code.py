1a7134f7d1fbb03c2d3294bc2db5e7aaa6009c86
Philip DeFraties <philipjfry338@gmail.com>
2021-02-21 23:01:22 -0700

    Adds reduce but grow challenge (python)

---
+++ reduce_but_grow_python/code.py
@@ -0,0 +1,5 @@
+def grow(list):
+    result = 1
+    for x in list:
+         result = result * x
+    return result 
---
+++ reduce_but_grow_python/test.py
@@ -0,0 +1,20 @@
+import unittest
+
+from code import grow
+
+
+class TestMethod(unittest.TestCase):
+    def test_list_int(self):
+        """
+        Test that it can sum a list of integers
+        """
+        result = grow([1,2,3])
+        self.assertEqual(result, 6)
+        result = grow([4,5,6])
+        self.assertEqual(result, 120)
+        result = grow([10,10,1])
+        self.assertEqual(result, 100)
+        result = grow([1,5,6,10])
+        self.assertEqual(result, 300)
+        result = grow([10,10,10,10,10,10])
+        self.assertEqual(result, 1000000)
