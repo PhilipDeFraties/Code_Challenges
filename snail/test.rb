3c11410b7ab2227d0c08224c9334c102da6a2295
Philip DeFraties <philipjfry338@gmail.com>
2021-02-25 22:24:16 -0700

    Renames test in snail

--- snail/test.rb
+++ snail/test.rb
@@ -1,7 +1,7 @@
 require 'minitest/autorun'
 require_relative './code'
 require 'pry'
-class PlaygroundTest < Minitest::Test
+class SnailTest < Minitest::Test
   def test_snail
     assert_equal [1, 2, 3, 6, 9, 8, 7, 4, 5], snail([ [1, 2, 3], [4, 5, 6], [7, 8, 9] ])
     assert_equal [1,2,3,4,8,12,16,15,14,13,9,5,6,7,11,10,], snail([ [1,2,3,4], [5,6,7,8], [9,10,11,12], [13,14,15,16] ])
