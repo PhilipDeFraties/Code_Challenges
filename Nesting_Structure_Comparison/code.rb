0c0d4957dc07e5e0dc773255d534f365cf0deae3
Philip DeFraties <philipjfry338@gmail.com>
2021-03-03 15:13:08 -0700

    updates with the actual solution submitted to codewars

--- Nesting_Structure_Comparison/code.rb
+++ Nesting_Structure_Comparison/code.rb
@@ -3,14 +3,16 @@ class Array
     if self.include?('[')
       return status
     end
-    if self.count != comp.count
+    if comp.class != Array
+      status = false
+    elsif self.count != comp.count
       status = false
     else
       self.each_with_index do |element, index|
         if element.class != comp[index].class
           status = false
         elsif element.class == Array
-          status = element.same_structure_as(comp[index])
+         status = element.same_structure_as(comp[index])
         else
           next
         end
