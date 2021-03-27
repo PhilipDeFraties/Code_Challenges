2f37a2d58eba9c76c1c1cb5c9f3f6387ba0c9f27
PhilipDeFraties <philipjfry338@gmail.com>
2021-03-26 23:52:43 -0600

    Adds grid spec file with placeholder tests

---
+++ p2p/spec/grid_spec.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require_relative './lib/grid'
+require 'pry'
+class GridTest < Minitest::Test
+  def test_it_exists
+
+  end
+
+  def test_it_has_attrs
+
+  end
+
+  def test_does_a_method
+
+  end
+
+  def test_does_other_methods
+
+  end
+end
