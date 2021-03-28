e30ee1ba9c143def1bc274e6090742fa413d048c
PhilipDeFraties <philipjfry338@gmail.com>
2021-03-28 00:58:43 -0600

    edits runner to call create_layout method

--- p2p/runner.rb
+++ p2p/runner.rb
@@ -1,5 +1,5 @@
-require './grid'
-require './bot'
+require './lib/grid'
+require './lib/bot'
 puts "Select any odd number from 3 to 99"
 m = gets.to_i
 
@@ -11,11 +11,11 @@ grid = Grid.new(m, princess_placement)
 
 grid.validate_size
 grid.validate_placement
+grid.create_layout
 
 puts "Here is your grid"
 grid.visualize_layout
 
 puts "To save the princess the robot takes the following steps:"
-bot = Bot.new(grid.create_layout)
-bot.save_princess
+bot = Bot.new(grid.layout)
 bot.path_to_princess
