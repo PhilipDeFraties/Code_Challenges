9f86cb2306d09d26965b01039dd13907dae5cd55
PhilipDeFraties <philipjfry338@gmail.com>
2021-03-28 00:57:29 -0600

    Adds tests for bot class

--- p2p/spec/bot_spec.rb
+++ p2p/spec/bot_spec.rb
@@ -1,20 +1,20 @@
 require 'minitest/autorun'
-require_relative './lib/bot'
+require './lib/bot'
+require './lib/grid'
 require 'pry'
 class BotTest < Minitest::Test
-  def test_it_exists
-    
+  def setup
+    grid = Grid.new(3, "TOP LEFT")
+    grid.create_layout
+    @bot = Bot.new(grid.layout)
   end
 
-  def test_it_has_attrs
-
+  def test_it_exists_with_attrs
+    assert_instance_of Bot, @bot
+    assert_equal [["p", "-", "-"], ["-", "b", "-"], ["-", "-", "-"]], @bot.grid
   end
 
-  def test_does_a_method
-
-  end
-
-  def test_does_other_methods
-
+  def test_it_gives_path_to_princess
+    assert_output("UP\nLEFT\n") { @bot.find_path_to_princess }
   end
 end
