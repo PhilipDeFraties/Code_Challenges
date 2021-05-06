81a75a11bda544c7737a5ce75581521f0849edb3
PhilipDeFraties <philipjfry338@gmail.com>
2021-05-05 23:38:39 -0600

    updates sum_pairs with efficient solution and additional test

--- sum_pairs/code.rb
+++ sum_pairs/code.rb
@@ -1,12 +1,34 @@
-def sum_pairs(array, sum)
+# Brute force version:
+# def sum_pairs(array, sum)
+#   results = []
+#   array.sort!
+#   array.each_with_index do |int, index|
+#     array.delete(int)
+#     array.each_with_index do |next_int, next_index|
+#       if int + next_int == sum
+#         results << [int, next_int].sort
+#       end
+#     end
+#   end
+#   return results.sort
+# end
+
+# O(n) version:
+def sum_pairs(nums, target)
   results = []
-  array.each_with_index do |int, index|
-    array.delete(int)
-    array.each_with_index do |next_int, next_index|
-      if int + next_int == sum
-        results << [int, next_int]
+  if nums.count < 2
+    return results
+  end
+  nums.sort!
+  (0..(nums.length - 2)).each do |first_index|
+    second_index = first_index + 1
+    until second_index == nums.length
+      sum = nums[first_index] + nums[second_index]
+      if sum == target
+        results << [nums[first_index], nums[second_index]].sort
       end
+      second_index += 1
     end
   end
-  return results
+  results.sort
 end
--- sum_pairs/test.rb
+++ sum_pairs/test.rb
@@ -8,7 +8,8 @@ class SumPairsTest < Minitest::Test
   # end
 
   def test_sum_pairs_finds_pairs
-    assert_equal [[6,3]], sum_pairs([2, 6, 3, 9, 11], 9)
-    assert_equal [[6,3], [2,7]], sum_pairs([2, 6, 3, 9, 11, 7], 9)
+    assert_equal [[3,6]], sum_pairs([2, 6, 3, 9, 11], 9)
+    assert_equal [[2,7], [3,6]], sum_pairs([2, 6, 3, 9, 11, 7], 9)
+    assert_equal [[-1, 8], [0, 7], [1, 6], [2, 5], [3, 4]], sum_pairs([1,6,2,5,7,8,-1,3,0,4], 7)
   end
 end
