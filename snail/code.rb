e4ab0081bc1cdd6ce28253c68534b60cef1b7e21
PhilipDeFraties <philipjfry338@gmail.com>
2021-05-05 23:05:56 -0600

    Update snail method to be more readable

--- snail/code.rb
+++ snail/code.rb
@@ -1,18 +1,10 @@
 def snail(array)
-  # creates clone of array so that original will be left untouched
-  array_clone = array.map(&:clone)
   new_array = []
-  until new_array.flatten.length == array_clone.flatten.length
-    new_array << array_clone.shift
-    array_clone.each {|array| new_array << array.pop } unless array_clone.empty?
-    new_array << array_clone.pop.reverse unless array_clone.empty?
-    if array_clone.empty?
-      break
-    else
-      array_clone.reverse.each do |array|
-        new_array << array.shift
-      end
-    end
+  until array.empty?
+    new_array << array.shift
+    array.each { |array| new_array << array.pop }
+    new_array << array.pop.reverse unless array.empty?
+    array.reverse.each { |array| new_array << array.shift }
   end
   return new_array.flatten
 end
